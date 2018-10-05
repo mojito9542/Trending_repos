ac7ec57761aadd0203756ff80f38a497741cec88
Carlos Henrique <carloshenry.wm@gmail.com>
2018-10-05 20:00:35 -0300

    Created a ListAdapter

---
+++ app/src/main/java/com/mrproducts/www/trending_repo/RepositoryListAdapter.java
@@ -0,0 +1,51 @@
+package com.mrproducts.www.trending_repo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositoryListAdapter extends RecyclerView.Adapter<RepositoryListAdapter.RepositoryViewHolder> {
+
+    private List<Repository> repositoryList = new ArrayList<Repository>();
+
+    @Override
+    public RepositoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_repository, parent, false);
+        return new RepositoryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RepositoryViewHolder holder, int position) {
+        holder.bind(repositoryList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return repositoryList.size();
+    }
+
+    public void update(List<Repository> data){
+        repositoryList.clear();
+        repositoryList.addAll(data);
+        notifyDataSetChanged();
+    }
+
+    class RepositoryViewHolder extends RecyclerView.ViewHolder{
+
+        RepositoryViewHolder(View view){
+            super(view);
+        }
+
+        void bind(Repository repository){
+            TextView itemTitle = itemView.findViewById(R.id.itemTitle);
+            TextView itemDescription = itemView.findViewById(R.id.itemDescription);
+            itemTitle.setText(repository.getTitle());
+            itemDescription.setText(repository.getDescription());
+        }
+    }
+}
